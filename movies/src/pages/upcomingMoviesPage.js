2020c1808293d7a177a1479ca67afd209b21aec2
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-10-22 15:06:39 +0100

    Added Upcoming movies page

---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner';
+
+const UpcomingMoviesPage = () => {
+  const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data.results;
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={(movie) => null}
+    />
+  );
+};
+
+export default UpcomingMoviesPage;
